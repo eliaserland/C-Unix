687310bdafd6b56078e7fb85e2c945fa3d0e4d22
Elias Olofsson <65086086+eliaserland@users.noreply.github.com>
2021-09-16 15:32:39 +0200

    Worked through F03-F04.

--- example_code/F03/myprog.c
+++ example_code/F03/myprog.c
@@ -1,19 +1,19 @@
-#include    <stdio.h>
-#include    <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-  if (argc != 1) {
-    fprintf(stderr ,"%s: Too many arguments (0 expected)\n", argv[0]);
-    return 1;
-  }
+    if (argc != 1) {
+        fprintf(stderr ,"%s: Too many arguments (0 expected)\n", argv[0]);
+        return 1;
+    }
 
-  printf("Utskrift nummer 1\n");
+    printf("Utskrift nummer 1\n");
 
-  fprintf(stdout, "Utskrift nummer 2\n");
-  fprintf(stderr, "Utskrift nummer 2.5\n");
+    fprintf(stdout, "Utskrift nummer 2\n");
+    fprintf(stderr, "Utskrift nummer 2.5\n");
 
-  write(STDOUT_FILENO, "Utskrift nummer 3\n", 18);
+    write(STDOUT_FILENO, "Utskrift nummer 3\n", 18);
 
-  return 0;
+    return 0;
 }
--- example_code/F03/pipe.c
+++ example_code/F03/pipe.c
@@ -11,100 +11,100 @@ void child2(int parent2child[], int child2child[], int child2parent[]);
 #define WRITE_END  1
 
 int main ( int argc, char *argv[] ) {
-  int    parent2child[2];
-  int    child2child[2];
-  int    child2parent[2];
-  pid_t  pid;
-  int    number, ret;
-  
-  /* Test of number of arguments (should be 0) */
-  if (argc != 1) {
-	fprintf(stderr, "\n%s takes no argument\n\n", argv[0]);
-    exit(1);
-  }
+    int    parent2child[2];
+    int    child2child[2];
+    int    child2parent[2];
+    pid_t  pid;
+    int    number, ret;
 
-  /* Open pipes  */
-  if (pipe(parent2child) != 0 ) {
-    perror("Pipe1");
-    exit(1);
-  }
-  if (pipe(child2child) != 0 ) {
-    perror("Pipe2");
-    exit(1);
-  }
-  if (pipe(child2parent) != 0 ) {
-    perror("Pipe3");
-    exit(1);
-  }
-  
-  /* Create a new process for child 1 */
-  pid = fork();
-  
-  if ( pid < 0 ) {
-    perror("fork 1\n");
-    exit(1);
-  } else if ( pid == 0 ) {      /* Child 1 */
-    child1(parent2child, child2child, child2parent);
-    exit(0);
-  }
-
-  /* Parent */
-  /* Create a new process for child 2 */
-  pid = fork();
-  
-  if ( pid < 0 ) {
-    perror("fork 2\n");
-    exit(1);
-  } else if ( pid == 0 ) {      /* Child 2 */
-    child2(parent2child, child2child, child2parent);
-    exit(0);
-  }
-
-
-  /* Parent */
-  int input = 0;
-
-  /* Close non used pipes */
-  close(parent2child[READ_END]);
-  close(child2child[READ_END]);
-  close(child2child[WRITE_END]);
-  close(child2parent[WRITE_END]);
-
-  /* Do the work */
-  do {
-    printf("Input an integer: ");
-    while ((input = scanf("%d", &number)) != 1) {
-      if (input == EOF)
-        break;
-      fprintf(stderr, "Conversion failed - must be an integer or EOF, try again\n");
-      getchar();
-    }
-    if (input != EOF) {
-      if (write(parent2child[WRITE_END], &number, sizeof(int)) == -1) {
-        perror("write to child 1");
+    /* Test of number of arguments (should be 0) */
+    if (argc != 1) {
+        fprintf(stderr, "\n%s takes no argument\n\n", argv[0]);
         exit(1);
-      }
-      ret = read(child2parent[READ_END], &number, sizeof(int));
-      if (ret == -1) {
-        perror("read from child 2");
-        exit(1);
-      }
-      printf("The answer is = %d\n\n", number);
     }
-  } while (input != EOF);
 
-  /* Work finished close the rest of the pipes */
-  close(parent2child[WRITE_END]);
-  close(child2parent[READ_END]);
+    /* Open pipes  */
+    if (pipe(parent2child) != 0 ) {
+        perror("Pipe1");
+        exit(1);
+    }
+    if (pipe(child2child) != 0 ) {
+        perror("Pipe2");
+        exit(1);
+    }
+    if (pipe(child2parent) != 0 ) {
+        perror("Pipe3");
+        exit(1);
+    }
+  
+    /* Create a new process for child 1 */
+    pid = fork();
 
-  /* Wait for both childs */
-  for (int i = 0; i < 2; i++)
-    if ((pid = wait(NULL)) == -1)
-      perror("Error waiting");
-    else
-      printf("Child %d has terminated\n", pid);
+    if (pid < 0) {
+        perror("fork 1\n");
+        exit(1);
+    } else if (pid == 0) {      /* Child 1 */
+        child1(parent2child, child2child, child2parent);
+        exit(0);
+    }
 
-  exit(0);
+    /* Parent */
+    /* Create a new process for child 2 */
+    pid = fork();
+  
+    if (pid < 0) {
+        perror("fork 2\n");
+        exit(1);
+    } else if (pid == 0) {      /* Child 2 */
+        child2(parent2child, child2child, child2parent);
+        exit(0);
+    }
+
+
+    /* Parent */
+    int input = 0;
+
+    /* Close non used pipes */
+    close(parent2child[READ_END]);
+    close(child2child[READ_END]);
+    close(child2child[WRITE_END]);
+    close(child2parent[WRITE_END]);
+
+    /* Do the work */
+    do {
+        printf("Input an integer: ");
+        while ((input = scanf("%d", &number)) != 1) {
+            if (input == EOF)
+                break;
+            fprintf(stderr, "Conversion failed - must be an integer or EOF, try again\n");
+            getchar();
+        }
+        if (input != EOF) {
+            if (write(parent2child[WRITE_END], &number, sizeof(int)) == -1) {
+                perror("write to child 1");
+                exit(1);
+            }
+            ret = read(child2parent[READ_END], &number, sizeof(int));
+            if (ret == -1) {
+                perror("read from child 2");
+                exit(1);
+            }
+            printf("The answer is = %d\n\n", number);
+        }
+    } while (input != EOF);
+
+    /* Work finished close the rest of the pipes */
+    close(parent2child[WRITE_END]);
+    close(child2parent[READ_END]);
+
+    /* Wait for both childs */
+    for (int i = 0; i < 2; i++)
+        if ((pid = wait(NULL)) == -1)
+            perror("Error waiting");
+        else
+            printf("Child %d has terminated\n", pid);
+
+    exit(0);
 }
 
 void child1(int parent2child[], int child2child[], int child2parent[])
@@ -118,16 +118,16 @@ void child1(int parent2child[], int child2child[], int child2parent[])
     close(child2parent[WRITE_END]);
 
     while ((ret = read(parent2child[READ_END], &temp_nr, sizeof(int))) == sizeof(int)) {
-      temp_nr++;
-      if (write(child2child[WRITE_END], &temp_nr, sizeof(int)) == -1) {
-        perror("write to child 2");
-        exit(1);
-      }
+        temp_nr++;
+        if (write(child2child[WRITE_END], &temp_nr, sizeof(int)) == -1) {
+            perror("write to child 2");
+            exit(1);
+        }
     }
 
     if (ret == -1) {
-      perror("read from parent");
-      exit(1);
+        perror("read from parent");
+        exit(1);
     }
 
     /* Work finished close the rest of the pipes */
@@ -148,16 +148,16 @@ void child2(int parent2child[], int child2child[], int child2parent[])
     close(child2parent[READ_END]);
 
     while ((ret = read(child2child[READ_END], &local_nr, sizeof(int))) == sizeof(int)) {
-      local_nr*=10;
-      if (write(child2parent[WRITE_END], &local_nr, sizeof(int)) == -1) {
-        perror("write to parent");
-        exit(1);
-      }
+        local_nr*=10;
+        if (write(child2parent[WRITE_END], &local_nr, sizeof(int)) == -1) {
+            perror("write to parent");
+            exit(1);
+        }
     }
 
     if (ret == -1) {
-      perror("read from child 1");
-      exit(1);
+        perror("read from child 1");
+        exit(1);
     }
 
     /* Work finished close the rest of the pipes */
