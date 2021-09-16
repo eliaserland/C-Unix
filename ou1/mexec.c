f7c82ec69e72ce3f0bd65b5ae7ed77f7598824b5
Elias Olofsson <65086086+eliaserland@users.noreply.github.com>
2021-09-16 15:33:48 +0200

    ou1: Makefile draft done, mexec skeleton created.

---
+++ ou1/mexec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) { //int argc, char *argv[]
+    
+    // Parse input arguments
+    
+    // Open all pipes required
+    
+    // Fork, create all childs
+    
+    // dup2
+    
+    // Close all unused pipe-ends 
+    
+    // Exec in each child
+    
+    // Wait
+    
+    return 0;
+}
