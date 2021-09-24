2352cd9e847e952d8e9b44b773e1ae2cfdbfe53e
Elias Olofsson <65086086+eliaserland@users.noreply.github.com>
2021-09-25 01:26:37 +0200

    ou2: much progress.

--- ou2/mmake.c
+++ ou2/mmake.c
@@ -15,13 +15,132 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <stdbool.h>
+#include <time.h>
 #include "parser.h"
-#include "stack.h"
 
 
-void exec_rule(makefile *make, const char *target, const char *argv[]);
-void exec_rule2(makefile *make, struct rule *r, const char *target, const char *argv[]); 
+//#include "stack.h"
+void print_cmd(char **cmd);
+time_t get_mtime(const char *file);
+bool file_exists(const char *file);
+makefile *open_and_parse_mkfile(const char *mkfile, bool custom_mkfile);
+void exec_cmd(char **cmd, bool silent);
+void exec_rule(makefile *make, struct rule *r, const char *target, const char *prgname, bool silent, bool force_build);
+
+
+// Get last modification time of file
+time_t get_mtime(const char *file) {
+	struct stat s;
+	if (stat(file, &s) == -1) {
+		perror("stat"); 
+		exit(EXIT_FAILURE);
+	}
+	return s.st_mtime;
+}
+
+// Returns true if file exists
+bool file_exists(const char *file) {
+	return access(file, F_OK) == 0;
+}
+
+
+/* Open and parse a makefile. */
+makefile *open_and_parse_mkfile(const char *mkfile, bool custom_mkfile) {   
+
+    FILE *mmakefp;
+    if (!custom_mkfile) {
+    	mkfile = "mmakefile";
+	} 
+    if ((mmakefp = fopen(mkfile, "r")) == NULL) {
+    	perror(mkfile);
+		exit(EXIT_FAILURE);
+    }
+    makefile *m = parse_makefile(mmakefp);
+    if (m == NULL) {
+    	fprintf(stderr, "%s: Could not parse makefile\n", mkfile);
+    	exit(EXIT_FAILURE);
+    }
+    fclose(mmakefp); 
+	return m;
+}
+
+void print_cmd(char **cmd) {
+	printf("%s", cmd[0]);
+	for (int i = 1; cmd[i] != NULL; i++) {
+		printf(" %s", cmd[i]);	
+	}
+	printf("\n");
+}
+
+
+void exec_cmd(char **cmd, bool silent) {
+	
+	if (!silent) {
+		print_cmd(cmd);		
+	}
+	pid_t pid = fork(); 		
+	if (pid < 0) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) { 
+		// Execute command in child process only
+		if (execvp(cmd[0], cmd) < 0) {
+			perror(cmd[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	// Parent
+	int status;
+	if (wait(&status) == -1){
+		perror("wait");
+ 		exit(EXIT_FAILURE);
+	}
+	if (WEXITSTATUS(status) != 0) {
+		exit(EXIT_FAILURE);
+ 	}
+}
+
+
+void exec_rule(makefile *make, struct rule *r, const char *target, const char *prgname, bool silent, bool force_build) {
+	// RULE r IS ASSUMED TO BE VALID AND EXISTING
+	
+	time_t mtime, mtime_newest = 0;
+	
+	// Get prerequisites.
+	const char **prereqs = rule_prereq(r);
+	
+	// For each prerequisite, run associated rule or check if file exists.  
+	for (int i = 0; prereqs[i] != NULL; i++) {
+		rule *prereq_rule = makefile_rule(make, prereqs[i]);
+		if (prereq_rule == NULL) {
+			// Check if prequisite file exists.
+			if (!file_exists(prereqs[i])) {
+				fprintf(stderr, "%s: No rule to make target '%s', " 
+				        "needed by '%s'.\n", prgname, prereqs[i], target);
+				exit(EXIT_FAILURE);
+			}
+			
+			mtime = get_mtime(prereqs[i]);
+			if (mtime > mtime_newest) {
+				mtime_newest = mtime;
+			} 
+		} else {
+			// Run the prerequisite rule.
+			exec_rule(make, prereq_rule, prereqs[i], prgname, silent, force_build);				
+		}
+		// Prerequisite rule has been run, or prereq file exists --> prereq satisfied.		
+	}
+	
+	char **cmd = rule_cmd(r);
+	
+	// if force_build OR target file does not exist OR any prereq file newer than target file {
+	if (force_build || !file_exists(target) || get_mtime(target) < mtime_newest) {
+		exec_cmd(cmd, silent);
+	} 
+}	
+
 
 
 int main (int argc, char *argv[]) {
@@ -37,7 +156,7 @@ int main (int argc, char *argv[]) {
     	switch(opt) {
     	case 'f':
     		if ((mkfile = strdup(optarg)) == NULL) {
-  				perror("strdup error");
+  				perror("strdup");
   				exit(EXIT_FAILURE);
     		}
     		custom_mkfile = true;
@@ -55,197 +174,72 @@ int main (int argc, char *argv[]) {
     	}
     }
     
+    /* Open and parse the makefile. */
+    makefile *m = open_and_parse_mkfile(mkfile, custom_mkfile);	
+	
+	//--------------------------------------
+    
+	/* Parse targets. */
+	//void parse_targets(argc, argv, target_cnt, targets)
     /* Parse custom targets. */
-    char **targets = NULL;
+    // argc, argv 
+    
+    //ret: target_cnt, targets 
+    
+    int target_cnt;
+    bool custom_targets;
+    char **targets;
     if (optind < argc) {
-    	targets = malloc((argc-optind)*sizeof(char*));
-    	if (targets == NULL) {
-    		perror("malloc error");
-			exit(EXIT_FAILURE);
-    	}
-    	for (int i = 0; i < argc-optind; i++) {
+    	custom_targets = true; 
+    	target_cnt = argc-optind;
+    } else {
+    	custom_targets = false;
+    	target_cnt = 1;
+    }
+    targets = malloc(target_cnt * sizeof(char*));
+	if (targets == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+    if (custom_targets) {
+    	for (int i = 0; i < target_cnt; i++) {
     		if ((targets[i] = strdup(argv[i+optind])) == NULL) {
-    			perror("strdup error");
+    			perror("strdup");
   				exit(EXIT_FAILURE);
     		}
     	}
     }
-        
-    /* Open and parse the mmakefile. */
-    FILE *mmakefp;
-    if (!custom_mkfile) {
-    	mkfile = "mmakefile";
-	} 
-    if ((mmakefp = fopen(mkfile, "r")) == NULL) {
-    	perror(mkfile);
-		exit(EXIT_FAILURE);
-    }
-    makefile *m = parse_makefile(mmakefp);
-    if (m == NULL) {
-    	fprintf(stderr, "%s: Could not parse makefile\n", mkfile);
-    	exit(EXIT_FAILURE);
-    }
-    fclose(mmakefp); 
-	
-    
-    const char *target = makefile_default_target(m);
-    
-    // create empty stack
-    
-    // if no custom targets:
-    	// place default target on stack
-    // else:
-    	// place all custom targets on stack
-   
-	
-	// while stack is not empty:
-		// top & pop a target from the stack (i.e. a rule)
-		// get rule associated with target 
-		
-		// if rule exist
-			// exec_rule()
-		// else
-			// Error message and continue (???)
-   
-   
-   // ----------------------
-   
-   
-    // while stack is not empty:
-    	
-    	// check target at the top of the stack
-    	
-    	// for all prereqs:
-    		// if rule exists, add to stack
-    		// if file  
-    	
-    	
-    	
-    	
-    	
-    	// take a target from the stack
-    	
-		// if target does not exist
-			// place all prereqs on stack		    	
-    	
-    	// get prereqs
-    	// for each prereq 
-    	
-    		// if does not exist
- 	   			// place prereq on the stack
-    		// if newer than target
-    			// place prereq on the stack
-    		// if force_build 
-    			// place prereq on the stack
-    			
-    		
-    
-    // ------------------------------------------
-        
-    // IF no custom targets
-    	// get default target
-    	
-    // Else
-    	// For each custom target
-    	
-    
-    // for each target
-    	
-    	// execute command if force_build OR prereq newer than target OR target does not exist
-    	
-    	
-    // TEST PRINTS
-    for (int i = 0; i < argc-optind; i++) {
-    	printf("targets[%d]=%s\n", i, targets[i]);
+	/* If no targets specified, get default target. */ 
+    if (!custom_targets) {
+		targets[0] = strdup(makefile_default_target(m));
+		if (targets[0] == NULL) {
+				perror("strdup");
+  				exit(EXIT_FAILURE);
+		}
     }
     
+    //----------------------------------------------
     
-    
+    /* For every target, run the associated rule. */
+    for (int i = 0; i < target_cnt; i++) {
+   		rule *r = makefile_rule(m, targets[i]);
+   		if (r == NULL) {
+   			fprintf(stderr, "%s: No rule to make target '%s'.\n", argv[0], targets[i]);
+			exit(EXIT_FAILURE);
+   		} 
+   		exec_rule(m, r, targets[i], argv[0], silent_mode, force_build);
+    }
     
     /* Free dynamically allocated memory */
     if (custom_mkfile) {
     	free(mkfile);
     }
-    if (targets != NULL) {
-		for (int i = 0; i < argc-optind; i++) {
-    		free(targets[i]);
-    	}
-    	free(targets);
-    }
+	for (int i = 0; i < target_cnt; i++) {
+		free(targets[i]);
+	}
+    free(targets);
     makefile_del(m);
 
     return 0;
 } 
-
-
-/*
-void exec_rule(makefile *make, const char *target, const char *argv[]) {
-	rule *r;
-	char *p;
-	const char **prereqs;
-	r = makefile_rule(make, target); 
-	if (r == NULL) {
-		// NO RULE WITH NAME
-		if (access(target, F_OK) != 0) {
-			fprintf(stderr, "%s: No rule to make target '%s'", argv[0], target);
-			exit(EXIT_FAILURE);
-		} else {
-			// NO RULE, BUT FILE EXIST	
-			// ---> 			
-		}
-	} else {
-		// RULE WITH NAME
-		// Get prerequisites of rule
-		prereqs = rule_prereq(r);
-		// For all prereqs:
-		for (int i = 0; prereqs[i] != NULL; i++) {
-			exec_rule(make, prereqs[i], argv);		
-		}
-		char **cmd = rule_cmd(r);
-		
-		if force_build OR target does not exist OR any prereq newer than target:
-			// Execute command
-				
-	}	
-}	
-*/
-
-
-
-
-void exec_rule2(makefile *make, struct rule *r, const char *target, const char *argv[]) {
-
-	// RULE r IS ASSUMED TO BE VALID AND EXISTING
-
-	// get prereqs of rule
-	const char **prereqs = rule_prereq(r);
-	
-	// for all prereqs:
-	for (int i = 0; prereqs[i] != NULL; i++) {
-		rule *prereq_rule = makefile_rule(make, prereqs[i]);
-		if (prereq_rule != NULL) {
-			// Rule for the prerequisite exists.
-			exec_rule2(make, prereq_rule, prereqs[i], argv);
-		} else {
-			if (access(prereqs[i], F_OK) != 0) {
-				// Prereq file not existing & also no rule to create it.
-				fprintf(stderr, "%s: No rule to make target '%s', " 
-				        "needed by '%s'.\n", argv[0], prereqs[i], target);
-				exit(EXIT_FAILURE);
-			}
-			// No rule, but prequisite file exist.	
-		}	
-		// The rule for the prereq has been run OR there was no rule, but the file exist.
-		// --> prereq satisfied		
-	}
-	// all prereqs satisfied
-	
-	char **cmd = rule_cmd(r);
-	/*	
-	if force_build OR target file does not exist OR any prereq file newer than target file {
-		// Execute command
-	}
-	*/
-}	
-
 
