b58300d72be5305b853b5d4a9ae3a8c06434d5ec
Elias Olofsson <65086086+eliaserland@users.noreply.github.com>
2021-09-30 23:24:47 +0200

    ou1: saving progress.

--- ou1/mexec.c
+++ ou1/mexec.c
@@ -19,14 +19,16 @@
 #define WRITE_END 1
 #define BUFSIZE 1024
 
-void close_pipes(int pipe_cnt, int **pipeID);
-int exec_cmd(char **args);
-int wait_on_children(int proc_cnt);
-void kill_all(int proc_cnt, int **pipeID, char ***prog_cmds);
+int  open_pipes			(int n, int ***pipeID);
+int  spawn_children		(int proc_cnt, int *child_no);
+int  dup_pipe			(int proc_cnt, int child_no, int **pipeID);
+void close_pipes		(int pipe_cnt, int **pipeID);
+int  exec_cmd			(char **args);
+int  wait_on_children	(int proc_cnt);
+void kill_all			(int proc_cnt, int **pipeID, char ***prog_cmds);
 
-
-int main (int argc, char *argv[]) {
-	
+int main (int argc, char *argv[]) 
+{	
 	FILE *input;
 	if (argc == 1) { 
 		// Read from stdin
@@ -91,74 +93,31 @@ int main (int argc, char *argv[]) {
 	
 	/* Open the (proc_cnt-1) pipes required. */
 	int **pipeID;
-	if (proc_cnt > 1) {
-		pipeID = malloc((proc_cnt-1)*sizeof(int*));
-		if (pipeID == NULL) {
-			perror("malloc error");
-			exit(EXIT_FAILURE);
-		}
-		for (int i = 0; i < proc_cnt-1; i++) {
-			pipeID[i] = malloc(2*sizeof(int));
-			if (pipeID[i] == NULL) {
-				perror("malloc error");
-				exit(EXIT_FAILURE);
-			}
-			if (pipe(pipeID[i]) != 0) {
-				perror("pipe error");
-				exit(EXIT_FAILURE);
-			}
-		}
+	if (proc_cnt > 1 && open_pipes(proc_cnt-1, &pipeID) != 0) {
+		// kill prog_cmds	
+		exit(EXIT_FAILURE);
 	}
-	
+		
 	// -------- Can use kill_all() from here on ------------
-	
-	spawn_children(proc_cnt)
 	
 	/* Create the (proc_cnt) children processes */
 	int child_no;
-	pid_t pid; 
-	for (int i = 0; i < proc_cnt; i++) {
-		pid = fork();
-		if (pid < 0) {
-			perror("fork error");
-			exit(EXIT_FAILURE);
-		} else if (pid == 0) { // Only children
-			/* Perform dup2 */
-			if (proc_cnt > 1) {	
-				if (i == 0) { 
-					// First child
-					if (dup2(pipeID[i][WRITE_END], STDOUT_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				} else if (i == proc_cnt-1) { 
-					// Last child
-					if (dup2(pipeID[i-1][READ_END], STDIN_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				} else { 
-					// Intermediate children
-					if (dup2(pipeID[i-1][READ_END], STDIN_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-					if (dup2(pipeID[i][WRITE_END], STDOUT_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				}
-			}
-			child_no = i; 
-			break;    	
-		}
+	if (spawn_children(proc_cnt, &child_no) != 0) {
+		kill_all(proc_cnt, pipeID, prog_cmds);
+		exit(EXIT_FAILURE);
 	}
+	
+	/* Duplicate pipe file descriptors to stdin/stdout appropriately for all children. */
+	if (child_no > -1 && dup_pipe(proc_cnt, child_no, pipeID) != 0) {
+		kill_all(proc_cnt, pipeID, prog_cmds);
+		exit(EXIT_FAILURE);
+	}  
 	
 	/* Close all pipes (parent & children). */
 	close_pipes(proc_cnt, pipeID);
 	   
 	/* Exec on all children. */
-	if (pid == 0 && exec_cmd(prog_cmds[child_no]) != 0) {
+	if (child_no > -1 && exec_cmd(prog_cmds[child_no]) != 0) {
 		kill_all(proc_cnt, pipeID, prog_cmds);
 		exit(EXIT_FAILURE);
 	} 
@@ -176,67 +135,112 @@ int main (int argc, char *argv[]) {
 	return 0;
 }
 
+
+/**
+ * Open a specified number of pipes.
+ *
+ * @param n			The number of pipes to create.
+ * @param pipeID	Pointer to the created 2D array of pipe file descriptors. 
+ * @return			0 upon successful creation of all pipes.
+ */
+int open_pipes(int n, int ***pipeID) 
+{
+	fprintf(stderr, "Hello1\n");
+	*pipeID = malloc(n * sizeof(int*));
+	if (*pipeID == NULL) {
+		perror("malloc");
+		fprintf(stderr, "Hello2\n");
+		goto err0;
+	}
+	fprintf(stderr, "Hello3\n");
+	int i;
+	for (i = 0; i < n; i++) {
+		*pipeID[i] = malloc(2 * sizeof(int));
+		if (*pipeID[i] == NULL) {
+			perror("malloc");
+			fprintf(stderr, "Hello4\n");
+			goto err1;
+		}
+		fprintf(stderr, "Hello5\n");
+		if (pipe(*pipeID[i]) != 0) {
+			fprintf(stderr, "Hello6\n");
+			perror("pipe");
+			goto err2;
+		}
+		fprintf(stderr, "Hello7\n");
+	}
+	return 0;
+
+err2:
+	free(*pipeID[i]);
+err1:
+	for (int j = i-1; j >= 0; j--) {
+		free(*pipeID[j]);
+	}
+	free(*pipeID);
+err0: 
+	return 1;
+}
+
 /**
  * Create a specified number of chilren processes.
  *
- * @param proc_cnt  The number of children to create.
- * @param child_no  A pointer to a .
+ * @param proc_cnt	The number of children to create.
+ * @param child_no	Used to set a unique identifer of the children, gives the 
+ *					relative order among the processes. Parent is set to -1.
+ * @return			0 upon successful creation of all children.
  */
-int spawn_children(int proc_cnt, int *child_no) {
-	pid_t pid;	
+int spawn_children(int proc_cnt, int *child_no) 
+{
+	pid_t pid;
+	*child_no = -1;	
 	for (int i = 0; i < proc_cnt; i++) {
 		pid = fork();
 		if (pid < 0) {
-			perror("fork error");
-			return 1
+			perror("fork");
+			return 1;
 		} else if (pid == 0) { // Only children
 			*child_no = i; 
-			break;
-		}	
-	}
+			break; 
+		}
+	}	
 	return 0;
 }
-	
-	/* Create the (proc_cnt) children processes */
-	int child_no;
-	pid_t pid; 
-	for (int i = 0; i < proc_cnt; i++) {
-		pid = fork();
-		if (pid < 0) {
-			perror("fork error");
-			exit(EXIT_FAILURE);
-		} else if (pid == 0) { // Only children
-			/* Perform dup2 */
-			if (proc_cnt > 1) {	
-				if (i == 0) { 
-					// First child
-					if (dup2(pipeID[i][WRITE_END], STDOUT_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				} else if (i == proc_cnt-1) { 
-					// Last child
-					if (dup2(pipeID[i-1][READ_END], STDIN_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				} else { 
-					// Intermediate children
-					if (dup2(pipeID[i-1][READ_END], STDIN_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-					if (dup2(pipeID[i][WRITE_END], STDOUT_FILENO) == -1) {
-						perror("dup2 error");
-						exit(EXIT_FAILURE);
-					}
-				}
-			}
-			child_no = i; 
-			break;    	
+
+/**
+ * For a given child process, duplicate pipe file descriptors to stdin and/or stdout.
+ *
+ * @param proc_cnt  The total number of children processes.
+ * @param child_no  Unique identifier of the child, giving the relative order.
+ * @param pipeID    2D array of pipe file descriptors.
+ * @return			0 opon successful creation of all pipes.
+ */
+int dup_pipe(int proc_cnt, int child_no, int **pipeID) 
+{
+	if (child_no == 0) {
+		// First child	
+		if (dup2(pipeID[child_no][WRITE_END], STDOUT_FILENO) == -1) {
+			goto err;
+		}
+	} else if (child_no == proc_cnt-1) {
+		// Last child
+		if (dup2(pipeID[child_no-1][READ_END], STDIN_FILENO) == -1) {
+			goto err;
+		}
+	} else {
+		// Intermediate children
+		if (dup2(pipeID[child_no-1][READ_END], STDIN_FILENO) == -1) {
+			goto err;
+		}
+		if (dup2(pipeID[child_no][WRITE_END], STDOUT_FILENO) == -1) {
+			goto err;
 		}
 	}
-
+	return 0;
+err:
+	perror("dup2");
+	return 1;
+}	
 
 /**
  * Close all pipes.
@@ -244,7 +248,8 @@ int spawn_children(int proc_cnt, int *child_no) {
  * @param pipe_cnt  The number of open pipes.
  * @param pipeID    2D array of pipe file descriptors.
  */
-void close_pipes(int pipe_cnt, int **pipeID) {
+void close_pipes(int pipe_cnt, int **pipeID) 
+{
 	for (int i = 0; i < pipe_cnt-1; i++) {
 		for (int j = 0; j < 2; j++) {
 			close(pipeID[i][j]);
@@ -258,11 +263,13 @@ void close_pipes(int pipe_cnt, int **pipeID) {
  * @param args	Array of strings, representing program command with arguments.
  * @return 		Returns 1 if exec fails.
  */
-int exec_cmd(char **args) {
+int exec_cmd(char **args) 
+{
 	if (execvp(args[0], args) < 0) {
 		perror(args[0]);
 		return 1;
-	}	
+	}
+	return 0; // This line is never executed, but the compiler complains if I remove it. 	
 }
 
 /**
@@ -271,11 +278,12 @@ int exec_cmd(char **args) {
  * @param proc_cnt  The number of children processes.
  * @return 			Returns 0 if successfully waited on all children.
  */
-int wait_on_children(int proc_cnt) {
+int wait_on_children(int proc_cnt) 
+{
 	int status;
  	for (int i = 0; i < proc_cnt; i++) {
  		if ((wait(&status)) == -1) {
- 			perror("wait error");
+ 			perror("wait");
  			return 1;
  		}
  		if (WEXITSTATUS(status) != 0) {
@@ -293,7 +301,8 @@ int wait_on_children(int proc_cnt) {
  * @param pipeID    2D array of pipe file descriptors.
  * @param prog_cmds 2D array of program commands.
  */
-void kill_all(int proc_cnt, int **pipeID, char ***prog_cmds) {
+void kill_all(int proc_cnt, int **pipeID, char ***prog_cmds) 
+{
 	/* Freeing dynamically allocated memory */ 
 	for (int i = 0; i < proc_cnt; i++) {
 		int j = 0; 
@@ -311,6 +320,4 @@ void kill_all(int proc_cnt, int **pipeID, char ***prog_cmds) {
 		free(pipeID);
 	}
 }
-
-
 
